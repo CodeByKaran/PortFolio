148acb0728247ea35f98f9f62534ae2044eef16b
karankumar010010@gmail.com <karankumar010010@gmail.com>
2024-01-06 01:45:13 +0530

    chng tic

--- Projects/Tic-Tac-Toe/main.js
+++ Projects/Tic-Tac-Toe/main.js
@@ -1,130 +1,163 @@
-let btn= document.querySelector("button")
-let select=document.getElementById("select")
-let scrn=document.querySelectorAll(".inp");
-let res=document.getElementById("result")
-let turn=document.getElementById("turn")
-let arr= Array.from(scrn)
-let player="X"
+const root = document.getElementById("root")
+const chnc=document.getElementById("chnc")
+const res=document.getElementById("res")
+const btnrest=document.getElementById("btnrest")
 
-function draw(play){
-  
 
-  
-  if( scrn[0].innerHTML!="" && scrn[1].innerHTML!="" && scrn[2].innerHTML!="" && scrn[3].innerHTML!= ""&& scrn[4].innerHTML!="" && scrn[5].innerHTML!="" && scrn[6].innerHTML!="" && scrn[7].innerHTML!="" && scrn[8].innerHTML!="" && scrn[0].style.color!="red"){
-     
-   res.innerHTML="MATCH DRAWN!"
-   
-   
- }
-}
- 
-let checkwin=(e,play)=>{
-  
-  if( scrn[6].innerHTML==play && scrn[7].innerHTML==play && scrn[8].innerHTML==play ||
-    scrn[3].innerHTML==play && scrn[4].innerHTML==play && scrn[5].innerHTML==play ||
-    scrn[2].innerHTML==play && scrn[4].innerHTML==play && scrn[6].innerHTML==play ||
-    scrn[0].innerHTML==play && scrn[4].innerHTML==play && scrn[8].innerHTML==play ||
-    scrn[0].innerHTML==play && scrn[1].innerHTML==play && scrn[2].innerHTML==play || scrn[0].innerHTML==play && scrn[3].innerHTML==play && scrn[6].innerHTML==play  || scrn[1].innerHTML==play && scrn[4].innerHTML==play && scrn[7].innerHTML==play  || scrn[2].innerHTML==play && scrn[5].innerHTML==play && scrn[8].innerHTML==play ){
-    
-   res.innerHTML=`PLAYER ${play} WIN!`;
-   turn.innerHTML="RESET TO PLAY AGAIN!"
-   
-   for(var i=0;i<arr.length;++i){
-     scrn[i].style.color="red"
-   scrn[i].innerHTML=play
-   }
-
- }
-  
-   draw(e)
-
+function Board(id) {
+  return { id }
 }
 
 
-arr.forEach(inp=>{
-  inp.addEventListener("click",(e)=>{
-    let b=e.target
-    let c=e.target.innerHTML
-    let a = e.target.innerHTML=player
-    
-    if(c=="X"){
-      e.target.innerHTML="X"
-       player=c
-       
-    }
-    
-    else if(c=="O"){
-      e.target.innerHTML="O"
-     player=c
-    
-    }
+function GameStart() {
+  return [
+  Board("box1"),
+  Board("box2"),
+  Board("box3"),
+  Board("box4"),
+  Board("box5"),
+  Board("box6"),
+  Board("box7"),
+  Board("box8"),
+  Board("box9")
+  ]
+}
 
-  
-    checkwin(a,player)
-     
-    
-    if (player=="X") {
-      player="O"
-    }
-    else{
-      player="X"
-    }
-    
-    
-    
-  
-    
-  turn.innerHTML=`PLAYER ${player} TURN!`
-    
-    
-    })
+let GameBoard = GameStart();
+
+GameBoard.forEach((e) => {
+  let input = document.createElement("div");
+  input.value = e.value;
+  input.id = e.id;
+  input.classList.add("inpbox");
+  root.appendChild(input);
 })
 
 
+const Box = document.querySelectorAll(".inpbox");
+
+let player = 'X'
 
 
-function rest(){
-  for(var i=0;i<arr.length;++i){
-     scrn[i].style.color="rgba(87, 69, 10, 1)"
-   scrn[i].innerHTML=""
+function RemoveEvent(id) {
+  document.getElementById(id).removeEventListener("click", Put_x_o);
+}
+
+
+function EndGame() {
+  Box.forEach((e) => {
+    e.removeEventListener("click", Put_x_o);
+  })
+}
+
+
+let DrawCheck=[]
+let GameWin_Not=false;
+
+
+
+function CheckWin(player) {
+  let P = player;
+  
+  for (let i = 0; i < Box.length; ++i) {
+
+    //Horizontal Check
+    if (Box[0].innerText == P & Box[1].innerText == P & Box[2].innerText == P || Box[3].innerText == P & Box[4].innerText == P & Box[5].innerText == P || Box[6].innerText == P & Box[7].innerText == P & Box[8].innerText == P) {
+      GameWin_Not=true;
+      EndGame();
+      res.innerText=`Player ${P} Won`
+      
+      
+    }
+
+    //vertical Check
+    if (Box[0].innerText == P & Box[3].innerText == P & Box[6].innerText == P || Box[1].innerText == P & Box[4].innerText == P & Box[7].innerText == P || Box[2].innerText == P & Box[5].innerText == P & Box[8].innerText == P) {
+      GameWin_Not=true;
+      EndGame();
+      res.innerText=`Player ${P} Won`
+    }
+    
+    //Digonal Check
+    if(Box[0].innerText == P & Box[4].innerText == P & Box[8].innerText == P || Box[2].innerText == P & Box[4].innerText == P & Box[6].innerText == P ){
+      GameWin_Not=true;
+      EndGame();
+      res.innerText=`Player ${P} Won`
+    }
+    
+    //DrawCheck
+    else if(DrawCheck.length>=8 & GameWin_Not==false){
+      EndGame();
+      return 0;
+    }
   }
-  turn.innerHTML="PLAYER TURNS"
-  res.innerHTML="RESULT"
+  
+ DrawCheck.push(P)
+ 
+  if (GameWin_Not==true) {
+    res.style.opacity="1"
+    return 1;
+  }
+ 
 }
 
 
-select.onchange=()=>{
-if (select.value==="1") {
-  btn.classList.remove("btn3")
-  btn.classList.remove("btn2")
-  for(var i=0;i<scrn.length;++i){
-  scrn[i].className="inp"
-  btn.classList.remove("btn1")
-}
-}
-else if(select.value==="2"){
-  btn.classList.remove("btn2")
-  btn.classList.remove("btn3")
-  btn.classList.add("btn1")
-  for(var i=0;i<scrn.length;++i){
-      scrn[i].classList.remove("board4")
-  scrn[i].classList.remove("board3")
-  scrn[i].classList.add("board2")
-}
-}
-else if(select.value==="3"){
-  btn.classList.remove("btn3")
-  btn.classList.add("btn2")
-for(var i=0;i<scrn.length;++i){
-  scrn[i].classList.remove("board4")
-  scrn[i].classList.add("board3") 
-}
-}
-else if(select.value==="4"){
-  btn.classList.add("btn3")
-for(var i=0;i<scrn.length;++i){
-  scrn[i].classList.add("board4") 
-}
-}
+
+function Put_x_o() {
+
+  let Span = document.createElement("span")
+
+  Span.innerHTML = player;
+
+  let BoxId = this.id;
+
+  document.getElementById(BoxId).appendChild(Span);
+
+
+  RemoveEvent(BoxId);
+
+  let ch=CheckWin(player);
+
+  if (player === 'X') {
+    Span.className = "xplayer"
+    player = 'O'
+  }
+  else {
+    Span.className = "yplayer"
+    player = 'X'
+  }
+ 
+ chnc.innerText=`Player ${player} Chance`
+ 
+ 
+ if(ch==1){
+   chnc.innerText=`Congratulations!`
+   btnrest.style.display="block"
+ }
+ if(ch==0){ 
+   chnc.innerText="Match Drawn!"
+   btnrest.style.display="block"
+ }
+ 
 }
 
+
+Box.forEach((e) => {
+  e.addEventListener("click", Put_x_o)
+})
+
+
+function reset(){
+  Box.forEach((e)=>{
+    e.innerHTML=""
+    e.addEventListener("click",Put_x_o)
+    e.classList.remove("winbox")
+  })
+  GameWin_Not=false;
+  DrawCheck=[];
+  res.style.opacity="0"
+  chnc.innerText="Player  Chance"
+  btnrest.style.display="none"
+}
+
+
+btnrest.addEventListener("click",reset);
